6dedafbaad0b4055cd4dbe1747b7c83eacb36f65
Shreyascr5 <shreyas050801@gmail.com>
2023-02-17 18:56:55 +0530

    completed js

--- exe.js
+++ exe.js
@@ -1,29 +1,77 @@
-var butStarting = document.querySelector("#start");
-var butStop = document.querySelector("#stop");
-var butReset = document.querySelector("#reset");
-var Minutes = document.getElementById("minutes");
-var Seconds = document.getElementById("seconds");
-var milliSeconds = document.getElementById("millisecs");
-
-
-var min = 00;
+var mins = 00;
 var sec = 00;
-var millisec = 00;
+var millsec = 00;
+
+var getMin = document.querySelector('#minutes');
+var getSec = document.querySelector('#seconds');
+var getMillSec = document.querySelector('#millisecs');
+var btnStart = document.querySelector('#start');
+var btnStop = document.querySelector('#stop');
+var btnReset = document.querySelector('#reset');
+var Setint;
+
+function timeid() {
+    millsec++;
+    if (millsec < 10) {
+        getMillSec.innerHTML = "0" + millsec;
+    }
+    if (millsec >= 10) {
+        getMillSec.innerHTML = millsec;
+
+    }
+    if (millsec > 100) {
+        sec++;
+        millsec = 0;
+        getMillSec.innerHTML = "0" + millsec;
+        if (sec < 10) {
+            getSec.innerHTML = "0" + sec;
+
+        }
+        if (sec >= 10) {
+            getSec.innerHTML = sec;
+        }
+        if (sec > 60) {
+            mins++;
+            sec = 0;
+            getSec.innerHTML = "0" + sec;
+            if (mins < 10) {
+                getMin.innerHTML = "0" + mins;
+
+            }
+            if (mins >= 10) {
+                getMin.innerHTML = mins;
+            }
 
 
-
-
-
-butStarting.addEventListener("onclick", startfun);
-
-function resetfun() {
-    min = 00;
-    sec = 00;
-    millisec = 00;
-    Minutes.innerHTML = min;
-    Seconds.innerHTML = sec;
-    milliSeconds.innerHTML = milliSec;
+        }
+    }
 }
 
+function timeStart() {
+    Setint = setInterval(timeid, 100);
 
-butReset.addEventListener("onclick", resetfun);
+}
+
+function timeStop() {
+    clearInterval(Setint);
+
+}
+
+function timeReset() {
+    clearInterval(Setint);
+    mins = "00";
+    sec = "00";
+    millsec = "00";
+    getMillSec.innerHTML = millsec;
+    getMin.innerHTML = mins;
+    getSec.innerHTML = sec;
+}
+if (btnReset) {
+    btnReset.addEventListener("click", timeReset);
+}
+if (btnStop) {
+    btnStop.addEventListener("click", timeStop);
+}
+if (btnStart) {
+    btnStart.addEventListener("click", timeStart);
+}
